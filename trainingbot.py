366bfd1da31a33bbf040d0a8f0bffa002d16259c
LestherLopez <98544813+LestherLopez@users.noreply.github.com>
2024-12-10 23:33:26 -0600

    update model

--- trainingbot.py
+++ trainingbot.py
@@ -3,17 +3,13 @@ import json
 import pickle
 import numpy as np
 import tensorflow as tf
-
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-# Descargar recursos necesarios de nltk si es necesario
-nltk.download('punkt')
-nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
 
-# Cargar los intents desde el archivo JSON
+
 intents = json.loads(open('intents.json').read())
 
 words = []
@@ -63,18 +59,21 @@ trainY = training[:, len(words):]
 
 # Crear el modelo
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(128, input_shape=(len(trainX[0]),), activation='relu'),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(512, input_shape=(len(trainX[0]),), activation='relu'),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(len(trainY[0]), activation='softmax')
 ])
 
+
+
 # Compilar el modelo
-sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+sgd = tf.keras.optimizers.SGD(learning_rate=0.0001, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 # Entrenar el modelo
-hist = model.fit(np.array(trainX), np.array(trainY), epochs=200, batch_size=5, verbose=1)
+hist = model.fit(np.array(trainX), np.array(trainY), epochs=500, batch_size=5, verbose=1)
 model.save('chatbot_model.h5', hist)
 print('Entrenamiento y guardado completados')
